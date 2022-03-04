30b5cf572816dd1c91c9b981d129d72ebb3f7a42
Alimaa <alimaahashim@gmail.com>
2022-03-04 14:25:58 +0000

    add constructor

--- car.js
+++ car.js
@@ -1,1 +1,10 @@
 //manufacturer, price and engine type.
+
+// class is Car
+// constructor for class Car
+const Car = function(manufacturer, price, engineType){
+    this.manufacturer = manufacturer;
+    this.price = price;
+    this.engineType = engineType;
+
+}
