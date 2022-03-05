cd7c970bbd5e8cc635dbac1f1c18b2100b54d567
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-03-05 09:10:43 +0000

    added dealership constructor

---
+++ dealership.js
@@ -0,0 +1,7 @@
+// name, maximum number of cars it can have and the cars currently in stock.
+
+const Dealership= function(name, maxCars, stock){
+    this.name = name;
+    this.maxCars = maxCars;
+    this.stock = stock;
+}
