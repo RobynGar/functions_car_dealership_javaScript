3d7414749c332c01cab31422d9d166846501cef9
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-03-05 13:23:36 +0000

    added method

--- dealership.js
+++ dealership.js
@@ -1,7 +1,18 @@
-// name, maximum number of cars it can have and the cars currently in stock.
+// Add methods to Dealership to:
+// Count the number of cars in stock
+// Add a car to stock
+// Return an array containing each car's manufacturer
+// Find all the cars from a given manufacturer
+// Find the total value of all the cars in stock.
 
-const Dealership= function(name, maxCars, stock){
+const Dealership= function(name, maxCars){
     this.name = name;
     this.maxCars = maxCars;
-    this.stock = stock;
+    this.stock = 0;
 }
+
+Dealership.prototype.countCarStock =function(stock){
+    
+}
+
+module.exports = Dealership;
